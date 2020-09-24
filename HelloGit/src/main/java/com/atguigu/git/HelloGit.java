53f8d8db9dd471d963f940184658f5e7a54a2759
apple <137841179@qq.com>
2020-09-24 22:02:36 +0800

    更新4

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
         System.out.println("Hello Git！");
         System.out.println("2020加油");
         System.out.println("张守富你是最棒的！");
+        System.out.println("分子在第8行添加");
     }
 }
